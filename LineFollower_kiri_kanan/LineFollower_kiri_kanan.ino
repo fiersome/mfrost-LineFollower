bc9b0c1c1ce30bb12d217143225fc11864ccffb8
Gibran Wisesa <gibranmfw@gmail.com>
2016-09-12 22:20:42 +0700

    corrected error, added more sensor

--- LineFollower_kiri_kanan/LineFollower_kiri_kanan.ino
+++ LineFollower_kiri_kanan/LineFollower_kiri_kanan.ino
@@ -9,6 +9,16 @@ const int pwmB = 1;
 // IR sensor
 const int sensor1 = A0;
 const int sensor2 = A1;
+const int sensor3 = A2;
+const int sensor4 = A3;
+const int sensor5 = A4;
+const int sensor6 = A5;
+
+// Speed constant
+const int normal = 130;
+const int stopM = 0;
+const int slow = 90;
+const int fast = 220;
 
 void setup() {
   // set motor pin
@@ -20,6 +30,10 @@ void setup() {
   // set IR sensor pin
   pinMode(sensor1, INPUT);
   pinMode(sensor2, INPUT);
+  pinMode(sensor3, INPUT);
+  pinMode(sensor4, INPUT);
+  pinMode(sensor5, INPUT);
+  pinMode(sensor6, INPUT);
 
   // for serial communication
   Serial.begin(9600);
@@ -32,38 +46,62 @@ void setup() {
 }
 
 void loop() {
-  float s1 = digitalRead(sensor1);
-  float s2 = digitalRead(sensor2);
+  int s1 = analogRead(sensor1);
+  int s2 = analogRead(sensor2);
+  int s3 = analogRead(sensor3);
+  int s4 = analogRead(sensor4);
+  int s5 = analogRead(sensor5);
+  int s6 = analogRead(sensor6);
   
-  Serial.print(s1 );
-  Serial.print("------ ");
+  Serial.println(s1);
   Serial.println(s2);
-  
-  if(s1 == HIGH && s2 == LOW) 
+  Serial.println(s3);
+  Serial.println(s4);
+  Serial.println(s5);
+  Serial.println(s6);
+  Serial.println("------ ");
+
+  if(s1 == HIGH) {
+    Serial.println("turn left");
+    turnLeft(stopM, fast);
+  }
+  else if(s2 == HIGH) {
+    Serial.println("turn left");
+    turnLeft(slow, fast);
+  }
+  else if(s3 == HIGH) 
   {
     Serial.println("turn left");
-    turnLeft();
+    turnLeft(slow, normal);
+  }
+  else if(s6 == HIGH) {
+   Serial.println("turn right");
+   turnRight(fast, stopM);
+  }
+  else if(s5 == HIGH) {
+   Serial.println("turn right");
+   turnRight(fast, slow);
   }
 
-  if(s2 == HIGH && s1 == LOW) 
-  {
-    Serial.println("turn right");
-    turnRight();
+  else if(s4 == HIGH) {
+   Serial.println("turn right");
+   turnRight(normal, slow);
   }
 
 }
 
-void turnRight() {
+// speed1 < speed2
+void turnRight(int speed1, int speed2) {
   digitalWrite(motorA, LOW);
-  analogWrite(pwmA, 90);
+  analogWrite(pwmA, speed1);
   digitalWrite(motorB, HIGH);
-  analogWrite(pwmB, 130);
+  analogWrite(pwmB, speed2);
 }
 
-void turnLeft() {
+// speed2 < speed1
+void turnLeft(int speed1, int speed2) {
   digitalWrite(motorA, HIGH);
-  analogWrite(pwmA, 130);
+  analogWrite(pwmA, speed2);
   digitalWrite(motorB, LOW);
-  analogWrite(pwmB, 90);
+  analogWrite(pwmB, speed1);
 }
-
